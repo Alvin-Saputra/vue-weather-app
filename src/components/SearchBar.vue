47771ee5672e3457d366741d410e5927498bebd4
alvinsaputra123 <alvin.saputra20162016@gmail.com>
2025-12-27 23:32:07 +0700

    adding emits on searchbar components and adding logicn to the weather view.vue

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,23 @@
 <script setup>
+import { ref } from 'vue';
+
+const city = ref('');
+
+const emit = defineEmits(['search-city']);
+const onSearch = () => {
+    if (!city.value) {
+        return;
+    }
+    emit('search-city', city.value);
+};
 
 </script>
 
 <template>
     <div class="join w-full max-w-md ">
-        <input type="text" placeholder="Search city..."
+        <input v-model="city" type="text" placeholder="Search city..."
             class="input input-bordered join-item w-full bg-gray-900 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-        <button class="btn   bg-blue-600 hover:bg-blue-700 text-white">
+        <button v-on:click="onSearch" class="btn   bg-blue-600 hover:bg-blue-700 text-white">
             🔍
         </button>
     </div>
--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -3,14 +3,39 @@
 import lottieAnim from '@/assets/animation/Thunderstorm.json'
 import SearchBar from '@/components/SearchBar.vue'
 import WeatherCardInfo from '@/components/WeatherCardInfo.vue'
+import WeatherModel from '@/models/WeatherModel';
+import { getWeatherByCity } from '@/service/weatherService';
+import { ref } from 'vue';
 
+const errorMessage = ref('');
+const temperature = ref('');
+const weatherCondition = ref('');
 
+// Ini fungsi callback yang Anda maksud.
+// Fungsi ini TETAP di Parent, tidak perlu di-pass ke Child.
+const handleSearch = async (cityName) => {
+  try {
+    errorMessage.value = '';
+    console.log(`Mencari cuaca untuk: ${cityName}`); // Debugging
+    
+    // Panggil Service
+    const data = await getWeatherByCity(cityName);
+    const weatherData =  WeatherModel(data); 
+    temperature.value = weatherData.temp;
+    weatherCondition.value = weatherData.description;
+
+    
+  } catch (error) {
+    errorMessage.value = 'Kota tidak ditemukan';
+
+  }
+};
 </script>
 
 <template>
     <div class="flex flex-col gap-5 min-h-screen ">
         <div class="flex justify-center mt-8">
-            <SearchBar></SearchBar>
+            <SearchBar @search-city="handleSearch"></SearchBar>
         </div>
         <div class="flex gap-5 m-20 items-center">
 
